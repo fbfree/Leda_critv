5ef9636ef553227322b6863cc5876ba308876f63
fbfree <25514523+fbfree@users.noreply.github.com>
2026-02-01 13:43:34 -0500

    Updated python codes
    Implemented UIC 776-2 limits to acceleration in get_eng_limits.py.
    Bug fix to error handling in critv.py
    Added additional soil types to critv.py

--- get_eng_limits.py
+++ get_eng_limits.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         print("Usage: get_eng_limits.py infile outfile\n")
         sys.exit(1)
 
-    index = 0
+    index = 3 # Calculate displacements at 3rd layer, under the rubber pads.
     infile = open(sys.argv[1],'r')
     lines = infile.readlines()
     infile.close()
@@ -29,26 +29,26 @@ if __name__ == '__main__':
     speed_effects[:,0] = velocities
 
     # Find the maximum ground deplacement as a function of velocity in x and z
-    speed_effects[:,1] = numpy.sqrt(numpy.max(displacement_arr[:,:,1]**2 + displacement_arr[:,:,2]**2,axis=1))
+    speed_effects[:,1] = numpy.sqrt(numpy.max(displacement_arr[:,:,1+index*2]**2 + displacement_arr[:,:,2+index*2]**2,axis=1))
 
     # Find the maximum ground acceleration as a function of velocity
-    extended_arr = numpy.concatenate((displacement_arr[:,-1,numpy.newaxis,:3],displacement_arr[:,:,:3],displacement_arr[:,1,numpy.newaxis,:3]), axis=1)
+    extended_arr = numpy.concatenate((displacement_arr[:,:,(0,1+index*2,2+index*2)][:,-1,numpy.newaxis,:],displacement_arr[:,:,(0,1+index*2,2+index*2)],displacement_arr[:,:,(0,1+index*2,2+index*2)][:,1,numpy.newaxis,:]), axis=1)
     acc_arr = numpy.absolute((2*extended_arr[:,1:-1,1:]-extended_arr[:,0:-2,1:]-extended_arr[:,2:,1:])/(displacement_arr[:,1,0,numpy.newaxis,numpy.newaxis]-displacement_arr[:,0,0,numpy.newaxis,numpy.newaxis])**2)
     speed_effects[:,2] = numpy.max(numpy.sqrt(numpy.absolute(acc_arr[:,:,0]**2+acc_arr[:,:,1]**2)),axis=1) * speed_effects[:,0]**2 # Not implemented: Consider a penalty factor for x acceleration vs. y acceleration
 
     # Find maximum strain as a function of velocity
     speed_effects[:,3] = numpy.max(numpy.absolute(displacement_arr[:,:,-1]),axis=1)
 
-    # Search through to find the lowest speed at which the peak ground acceleration exceeds 3.5 m/s^2.  If no limit is found, report the maximum speed given in the data.
+    # Search through to find the lowest speed at which the peak ground acceleration exceeds 0.35 m/s^2 per UIC 776-2 specifications.  If no limit is found, report the maximum speed given in the data.
     max_acc = speed_effects[-1,0]
     for i,a in enumerate(speed_effects[:,2]):
-        if a > 3.5:
+        if a > 0.35:
             if i:
                 max_acc = speed_effects[i-1,0]
             else:
                 max_acc = 0
             break
-    print("Maximum velocity to maintain surface acceleration under 3.5 m/s2 is %f m/s \n" % max_acc)
+    print("Maximum velocity to maintain surface acceleration under 0.35 m/s2 is %f m/s \n" % max_acc)
 
     # Search through to find the lowest speed at which the peak ground strain exceeds 1.5e-4
     max_strain = speed_effects[-1,0]
